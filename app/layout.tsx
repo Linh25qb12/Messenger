6aa46aa59c0911ff6009078608d8a3b750620df6
TerminatorxxD <tuanlinhdoan2@gmail.com>
2023-05-26 03:39:50 +0700

    login/register route

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,25 @@
+import ToasterContext from './context/ToasterContext'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Messenger',
-  description: 'Texting Application',
+    title: 'Messenger',
+    description: 'Texting Application',
 }
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <ToasterContext />
+                {children}
+            </body>
+        </html>
+    )
 }
